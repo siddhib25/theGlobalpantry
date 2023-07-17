a9302fbaadf20adee699cbc69cc500e3192081f8
siddhi <bsiddhi01@gmail.com>
2023-07-17 14:25:13 -0400

    Initial Commit

---
+++ theGlobalpantry/theGlobalpantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  theGlobalpantryApp.swift
+//  theGlobalpantry
+//
+//  Created by Siddhi B on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct theGlobalpantryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
