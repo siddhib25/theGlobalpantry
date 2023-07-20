912239711e76f6835daa935039293502c587b213
CatherWang <139564734+CatherWang@users.noreply.github.com>
2023-07-20 10:50:04 -0700

    added the Australia recipe

--- theGlobalpantry/australiaRecipe.swift
+++ theGlobalpantry/australiaRecipe.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct australiaRecipe: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image("rice")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                
+        }
     }
 }
 
